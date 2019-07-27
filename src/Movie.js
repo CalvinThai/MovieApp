11736a55b9059840773beda32692ce5e4760883c
calvinthai <calvinthai97@hotmail.com>
2019-07-27 02:21:15 -0400

    added go back button

--- src/Movie.js
+++ src/Movie.js
@@ -1,5 +1,5 @@
 import React from 'react';
-
+import HomePage from './HomePage';
 class Movie extends React.Component{
   constructor(props){
     super(props)
@@ -8,25 +8,35 @@ class Movie extends React.Component{
       image: props.poster_path,
       plot_synopsis: props.plot_synopsis,
       user_rating: props.user_rating,
-      release_date: props.release_date
-
-
+      release_date: props.release_date,
+      job: 0
     };
+    this.page = null;
+  }
+
+  handleClick(e){
+    this.setState({job:1});
+    this.page = <HomePage />
+
   }
 
   //title, image, plot synopsis, user rating ,release date
 
   render(){
+    if (this.state.job===0){
+      this.page = (
+        <div>
+          <button onClick={()=> this.handleClick()}>Go Back</button>
+          <h1> {this.state.title} </h1>
+          <img src = {this.state.image} alt="new" />
+          <h1> {this.state.plot_synopsis} </h1>
+          <h1> {this.state.user_rating} </h1>
+          <h1> {this.state.release_date} </h1>
+        </div>
+      )
+    }
     return(
-      <div>
-      <h1> {this.state.title} </h1>
-      <img src = {this.state.image} alt="new" />
-      <h1> {this.state.plot_synopsis} </h1>
-      <h1> {this.state.user_rating} </h1>
-      <h1> {this.state.release_date} </h1>
-
-
-      </div>
+      this.page
     )
   }
 }
