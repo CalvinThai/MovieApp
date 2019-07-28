80c2fcb2564c33cf00a0037ac6f92fd20c2fbd01
calvinthai <calvinthai97@hotmail.com>
2019-07-28 16:05:12 -0400

    removed api key

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import HomePage from './HomePage';
 import './App.css';
-//key: 5d42ffc91983f9505c131b035b679cb8   "https://api.themoviedb.org/3/movie/popular?api_key=5d42ffc91983f9505c131b035b679cb8&language=en-US&page=1"
 class App extends React.Component{
   constructor(props){
     super(props);
